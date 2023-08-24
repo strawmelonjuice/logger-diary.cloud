ff82643b6d3f4369ddda19584ce1179ff1925083
Mar <mar@strawmelonjuice.com>
2023-08-24 13:17:12 +0200

    We get a little WOO WOO

--- client/modules/pageload.js
+++ client/modules/pageload.js
@@ -1,38 +1,68 @@
 // import pageLoader from "./modules/pageload.js";
-let pagecontent = document.getElementById('content');
+let pagecontent = document.getElementById("content");
+let setPageStyling = function (PageStyling) {
+    if (document.getElementById("contentstyling") != null) {
+        (document.getElementById("contentstyling")).remove;
+    }
+    var pageStyleSheet = document.createElement("style");
+    pageStyleSheet.innerHTML = PageStyling;
+    document.head.appendChild(pageStyleSheet);
+}
+// let pagestyling = ;
 export default () => {
-    function LoadPageModule(pagemodule,type = "snippet") {
-        import("./pages/" + pagemodule)
-            .then((page) => {
-                if (type === "string") {
-                pagecontent.innerHTML = page.default;} 
-                if (type === "snippet") {
-                  pagecontent.innerHTML = page.default();
-                } 
+    function LoadPageModule(pagemodule, type = "snippet") {
+        import("./pages/" + pagemodule).then((page) => {
+            if (type === "string") {
+                pagecontent.innerHTML = page.default;
             }
-            );
+            if (type === "snippet") {
+                pagecontent.innerHTML = page.default();
+            }
+        });
     }
     console.log("Page actualisation check");
     if (window.location.hash != pagecontent.dataset.currentView) {
         console.log("Actualising...");
         switch (window.location.hash) {
-            case '#/login/':
-                LoadPageModule("login.js","string")
-                pagecontent.dataset.currentView = '#/login/';
+            case "#/login/":
+                setPageStyling(`
+                body {
+                    background-color: pink;
+                }
+                `)
+                pagecontent.innerHTML = `
+                <form id="loginform" action="#/gologin/" method="get">
+                    <div class="form-group">
+                        <label><h4>Username</h4></label>
+                            <input type="text" name="username" id="username-input" class="LoginForm " value="Just_Mar_Ok">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-group">
+                        <label><h4>Password</h4></label>
+                            <input type="password" name="password" id="password-input" class="LoginForm " value="xxosihH8riEq7c">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-group"><br></br>
+                        <input type="submit" class="LoginForm LoginButton" value="Continue!">
+                    </div>
+                    <p>Don't have an account? <a href="https://logger-diary.online/register/" target="_blank">Sign up online</a>.</p>
+                </form>
+                `;
+                pagecontent.dataset.currentView = "#/login/";
                 break;
-            case '#/gologin/':
+            case "#/gologin/":
                 break;
-            case '#/offline/':
-                LoadPageModule("offline.mjs")
-                pagecontent.dataset.currentView = '#/offline/';
+            case "#/offline/":
+                LoadPageModule("offline.mjs");
+                pagecontent.dataset.currentView = "#/offline/";
                 break;
             default:
-            case '#/home/':
-                LoadPageModule("home.js","string");
-                window.location.hash = '/home/';
-                pagecontent.dataset.currentView = '#/home/';
+            case "#/home/":
+                LoadPageModule("home.js", "string");
+                window.location.hash = "/home/";
+                pagecontent.dataset.currentView = "#/home/";
                 break;
         }
         // ReLoadLang()
     }
-}
+};
